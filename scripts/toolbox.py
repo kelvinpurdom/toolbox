ab6b56443cafc96a4a14a631cefb2376127071fd
Kelvin Purdom <kelvin.purdom@gmail.com>
2022-05-23 14:49:41 +0200

    updated func

--- scripts/toolbox.py
+++ scripts/toolbox.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import train_test_split
 
-def train_test_split(df, testsize):
+def train_test_split_func(df, testsize):
 
     X = df.drop(columns= df[-1])
     y = df[-1]
