15ee3faa95331573161d57dddd1fe02c2dcdcfb3
Kelvin Purdom <kelvin.purdom@gmail.com>
2022-05-23 15:19:28 +0200

    Updated first func

--- toolbox.py
+++
@@ -1,8 +0,0 @@
-from sklearn.model_selection import train_test_split
-
-def train_test_split_func(df, testsize):
-
-    X = df.drop(columns= df[-1])
-    y = df[-1]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testsize)
-    return X_train, X_test, y_train, y_test
