ef3c515c8bc612ccd0e7e224cb70e632587d47e1
Kelvin Purdom <kelvin.purdom@gmail.com>
2022-05-23 14:47:12 +0200

    test push

---
+++ scripts/toolbox.py
@@ -0,0 +1,8 @@
+from sklearn.model_selection import train_test_split
+
+def train_test_split(df, testsize):
+
+    X = df.drop(columns= df[-1])
+    y = df[-1]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testsize)
+    return X_train, X_test, y_train, y_test
